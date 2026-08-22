6b517b66c5b30a78aeba620ee5a740d03db7ca3b
idandiher <andi.hermanto@jd.id>
2026-08-22 17:11:44 +0800

    Add Spring Boot 4.1.1 skeleton on Java 25

---
+++ src/test/java/com/singapore/weather/WeatherApplicationTests.java
@@ -0,0 +1,12 @@
+package com.singapore.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
